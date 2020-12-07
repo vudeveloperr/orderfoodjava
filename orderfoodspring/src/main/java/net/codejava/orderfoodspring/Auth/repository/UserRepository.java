805bf17f20043adc05a830ff84405a1731ebf587
NguyenVanVu2449 <ngvanvu2449@gmail.com>
2020-12-07 16:34:10 +0700

    add : update user

--- orderfoodspring/src/main/java/net/codejava/orderfoodspring/Auth/repository/UserRepository.java
+++ orderfoodspring/src/main/java/net/codejava/orderfoodspring/Auth/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package net.codejava.orderfoodspring.Auth.repository;
 
 import java.util.Optional;
+import java.beans.Transient;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.transaction.annotation.Transactional;
 import net.codejava.orderfoodspring.Auth.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -14,7 +16,13 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	Boolean existsByEmail(String email);
 	Optional<User> findById(Integer id);
 
-	@Modifying
+	@Transactional
+	@Modifying(clearAutomatically = true)
 	@Query(value = "UPDATE users set username = ?2, password = ?3, email= ?4 where id= ?1", nativeQuery = true)
-    List<User> updateUser(Integer id, String username,String password,String email);
+	void updateUser(Integer id, String username,String password,String email);
+	
+	@Transactional
+	@Modifying(clearAutomatically = true)
+	@Query(value = "UPDATE user_roles set role_id = ?2 where user_id = ?1", nativeQuery = true)
+	void updateRoleUser(Integer user_id, Integer role_id);
 }
