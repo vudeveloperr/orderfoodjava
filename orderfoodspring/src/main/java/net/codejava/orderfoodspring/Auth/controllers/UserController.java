952051e8a8beb48ef9198fd3eacf98b4c4041a0b
NguyenVanVu2449 <ngvanvu2449@gmail.com>
2020-12-07 15:01:29 +0700

    quan ly user

---
+++ orderfoodspring/src/main/java/net/codejava/orderfoodspring/Auth/controllers/UserController.java
@@ -0,0 +1,111 @@
+package net.codejava.orderfoodspring.Auth.controllers;
+
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import jdk.jfr.internal.Repository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import net.codejava.orderfoodspring.Auth.models.User;
+import net.codejava.orderfoodspring.Auth.models.ERole;
+import net.codejava.orderfoodspring.Auth.models.Role;
+import net.codejava.orderfoodspring.Auth.payload.request.UpdateUserRequest;
+import net.codejava.orderfoodspring.Auth.payload.response.MessageResponse;
+import org.springframework.http.HttpStatus;
+import net.codejava.orderfoodspring.Auth.security.services.UserDetailsServiceImpl;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import net.codejava.orderfoodspring.Auth.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.access.prepost.PreAuthorize;
+import net.codejava.orderfoodspring.Auth.repository.RoleRepository;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserDetailsServiceImpl service;
+
+    @Autowired
+    UserRepository userRepository;
+    
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/users")
+    public List<User> listuser(){
+        return service.listAll();
+    }
+
+    // @PutMapping("/users/{id}")
+    // public ResponseEntity<?> update(@RequestBody UpdateUserRequest userupdate, @PathVariable Integer id){
+    //     // System.out.println(userRepository.findById(id).toString());
+    //     if (userRepository.existsByUsername(userupdate.getUsername())) {
+    //         Set<String> strRoles = userupdate.getRole();
+    //         Set<Role> roles = new HashSet<>();
+    //         if (strRoles == null) {
+    //             Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+    //                     .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+    //             roles.add(userRole);
+    //         } else {
+    //             strRoles.forEach(role -> {
+    //                 switch (role) {
+    //                     case "admin":
+    //                         Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+    //                                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+    //                         roles.add(adminRole);
+    
+    //                         break;
+    //                     case "mod":
+    //                         Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+    //                                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+    //                         roles.add(modRole);
+    
+    //                         break;
+    //                     default:
+    //                         Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+    //                                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+    //                         roles.add(userRole);
+    //                 }
+    //             });
+    //         }
+    //         System.out.println(id);
+    //         System.out.println(userupdate.getUsername());
+    //         System.out.println(userupdate.getPassword());
+    //         System.out.println(userupdate.getEmail());
+    //         userRepository.updateUser(id, userupdate.getUsername(), encoder.encode(userupdate.getPassword()), userupdate.getEmail());
+    //         // user.setRoles(roles);
+    //         // service.save(user);
+    //         // return new ResponseEntity<User>(user, HttpStatus.OK);
+    //         return new ResponseEntity<UpdateUserRequest>(userupdate, HttpStatus.OK);
+    //     }else{
+    //         return ResponseEntity
+    //             .badRequest()
+    //                 .body(new MessageResponse("Error: Username is not found!!!"));
+    //     }
+    // }
+    
+    @PutMapping("/user/{id}")
+    public ResponseEntity<?> deleterole(@RequestBody User user, @PathVariable Integer id){
+        if (userRepository.existsByUsername(user.getUsername())) {
+            encoder.encode(user.getPassword());
+            service.save(user);
+            return new ResponseEntity<>("Success: Delete Role!!!", HttpStatus.OK);
+        }else{
+            return ResponseEntity
+                .badRequest()
+                    .body(new MessageResponse("Error: Username is not found!!!"));
+        }
+    }
+}
--- orderfoodspring/src/main/java/net/codejava/orderfoodspring/Auth/repository/UserRepository.java
+++ orderfoodspring/src/main/java/net/codejava/orderfoodspring/Auth/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package net.codejava.orderfoodspring.Auth.repository;
 
 import java.util.Optional;
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 import net.codejava.orderfoodspring.Auth.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -11,4 +12,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	Boolean existsByUsername(String tendn);
 	Boolean existsByEmail(String email);
+	Optional<User> findById(Integer id);
+
+	@Modifying
+	@Query(value = "UPDATE users set username = ?2, password = ?3, email= ?4 where id= ?1", nativeQuery = true)
+    List<User> updateUser(Integer id, String username,String password,String email);
 }
